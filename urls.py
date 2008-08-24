a96ad341daeada34f9880f93c264215dfbe2edab
Bruno Michel <bmichel@menfin.info>
2008-08-24 18:49:30 +0200

    Initial commit

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^dont_break_the_chain/', include('dont_break_the_chain.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
